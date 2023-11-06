f3c0585fc2e57172fccb90864869e8f562a49a0c
Ariel Halili <arielhalili259@gmail.com>
2023-11-06 19:08:12 +0200

    update server

--- ListViewer/server.js
+++ ListViewer/server.js
@@ -1,38 +1,22 @@
 const express = require('express');
-const fs = require('fs');
 
 const app = express();
 const port = 1337;
 
-const index_html = fs.readFileSync('./public/index.html', 'utf8');
-
+const index_html = require('fs').readFileSync('./public/index.html', 'utf8');
 var dataList = [];
 
-function onGET(req, res) {
-	res.send(index_html);
-}
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('./public/'));
 
-function onPOST(req, res) {
+app.post('/', function onPOST(req, res) {
 	if (req.body['message'] != '') {
 		dataList.push(req.body['message']);
 
-		var list_html = '';
-		for (var i = 0; i < dataList.length; i++) {
-			list_html += `<li>${dataList[i]}</li>`;
-		}
-
-		res.send(index_html.replace('<!-- DATA -->', list_html));
+		res.send(index_html.replace('<!-- DATA -->',
+			dataList.map((item) => `<li>${item}</li>`).join('')));
 	}
-}
+});
 
-
-//app.use(express.static(__dirname + '/public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(express.static('public'));
-
-app.post('/', onPOST);
-
-const server = app.listen(port);
-console.log(`app listening on port ${port}!`);
+app.listen(port);
+console.log(`app is running at localhost:${port}`);
